deb1ecf1edbe9bc30e202adac81a71c83082c88e
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2024-04-23 11:32:02 -0500

    Cria rota para rotas inexistentes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import "reflect-metadata";
 import router from "./routes";
 import { AppDataSource } from "./database/typeorm/data-source";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 AppDataSource.initialize();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,8 @@ const router = Router();
 
 router.use('/user', user);
 
+router.all('*', (req, res) => {
+    return res.status(404).json('Rota inexistente!');
+});
+
 export default router;
