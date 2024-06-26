e617fbb009be0a90333fbae282c596e9872d3eea
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2024-06-26 13:56:56 -0500

    Cria componentes base do header e footer

--- finance-controller-app/src/pages/home/home.tsx
+++ finance-controller-app/src/pages/home/home.tsx
@@ -1,17 +1,20 @@
-import useAuth from "../../hooks/useAuth";
+import Footer from "../../components/footer/footer";
+import Header from "../../components/header/header";
+// import useAuth from "../../hooks/useAuth";
 
 function Home() {
-    const { signOut }: any = useAuth();
+    // const { signOut }: any = useAuth();
 
-    const handleSignOut = async () => {
-        await signOut();
-    }
+    // const handleSignOut = async () => {
+    //     await signOut();
+    // }
 
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Home</h1>
-            <hr />
-            <span onClick={handleSignOut}>Sair</span>
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            {/* <span onClick={handleSignOut}>Sair</span> */}
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem adipisci minus incidunt perspiciatis ut accusantium iste, quo qui laboriosam eligendi blanditiis molestiae amet omnis! Maxime, eos? Doloribus deleniti aspernatur praesentium.</span>
+            <Footer />
         </div>
     )
 }
